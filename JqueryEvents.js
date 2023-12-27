ae8168b259ae1d90467f98d92e99ac401e646987
harishwarsj <harishsj1006@gmail.com>
2023-12-27 18:25:27 +0530

    Commited on 27.12.2023

---
+++ JqueryEvents.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+    // focus
+    $("#input").focus(function(){
+        $(this).css("background-color" , "black");
+    });
+    // blur
+    $("#input").blur(function(){
+        alert ("Event focus lost and blur occured!!");
+   });
+    //change
+    $("input").change(function(){
+        $(this).css("background-color" , "yellow");
+    });
+    // click
+    $(".button1").click(function(){
+        alert("the button is clicked");
+    });
+    // dbclick
+    $(".button2").dblclick(function(){
+        alert("the button2 is double cliked")
+    });
+// hover method
+    $("#hoverbtn").hover(function(){
+        $(this).css("background-color" , "yellow")},
+        function(){
+            $(this).css("color" , "green"
+            );
+    })
+
+// Focus in and focus out
+    $("div").focusin(function(){
+        $(this).css("background-color" , "lightgrey");
+        });
+    $("div").focusout(function(){
+        $(this).css("background-color" , "white");
+    });
+    
+    // on event and off event
+    $("#onevent").on("click" , function(){
+        $(this).css({ "background-color" : "Green" , "color" : "white"} );
+        alert("click event triggered");
+    });
+    $("#offevent").click(function(){
+        $("#onevent").off("click");
+    });
+    // one event
+
+    $(".oneevent").one("click dblclick" , function(){
+        $(this).animate({fontSize:"+=6px"});
+    });
+
+    // resize event
+let x = 0 ;
+    $(window).resize(function(){
+        $("#spanresize").text(x+=1)
+    });
+    // scroll event
+let a = 0;
+    $("scroll").scroll(function(){
+        $("span").text(a+=1);
+    });
+    });
+
+
---
+++ Jqueryselectors.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".selectorbtn").click(function(){
+        $("h1").hide();
+        $("#h2").hide();
+        $(".h2-2").hide();
+    })
+})
+
+
